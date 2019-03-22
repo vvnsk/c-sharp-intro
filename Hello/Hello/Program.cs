7b8d9fb8bc67ea866f763c6dddc8791b8d4187de
Saikrishna <vinnakotavnsaikrishna@gmail.com>
2019-03-21 21:41:09 -0500

    Hello World - C# Init

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Hello
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            // Console.WriteLine("Hello" + " World!");
+
+            // Console.WriteLine("Hello " + args[0]);
+
+            Console.WriteLine("Your Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name + "!");
+
+            if(hoursOfSleep > 8) {
+                Console.WriteLine("You are well rested");
+            } else {
+                Console.WriteLine("You need more sleep");
+            }
+        }
+    }
+}
