e13e3a8b14340dab6c48d0fdc9ff2d86340b4862
Saikrishna Vinnakota <vinnakotavnsaikrishna@gmail.com>
2019-05-16 21:29:50 -0500

    Entity Classes and Business Logic Layer

---
+++ ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Foo",
+                LastName = "Bar"
+            };
+            string expected = "Bar, Foo";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ACM/Tests/ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c37e075d-057c-46f8-88d5-825d551d1467")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
